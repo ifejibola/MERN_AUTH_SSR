192b4064da0afa2a245f56c31ff7c7ad91384cc0
Ife Ajibola <Ife@Ifes-MacBook-Air.local>
2020-06-03 21:08:56 -0400

    deployment ready

---
+++ client/core/Home.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Card, { CardContent, CardMedia } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import SADE from '../assets/images/shellByLake.jpg'
+
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+})
+
+class Home extends Component {
+  render() {
+    const { classes } = this.props
+    return (
+      <Card className={classes.card}>
+        <Typography type="headline" component="h2" className={classes.title}>
+          Home Page
+          </Typography>
+        <CardMedia className={classes.media} image={SADE} title="Unicorn Shells" />
+        <CardContent>
+          <Typography type="body1" component="p">
+            Welcome to the MERN SSR home page.
+            </Typography>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Home)
